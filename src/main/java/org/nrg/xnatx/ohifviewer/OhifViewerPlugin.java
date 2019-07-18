e0ec3d32e039660f266a82add8ef92678678643a
jpetts <jpetts@12136phydt.icr.ac.uk>
2019-07-18 12:16:19 +0100

    Up RC number displayed in viewer.

--- src/main/java/org/nrg/xnatx/ohifviewer/OhifViewerPlugin.java
+++ src/main/java/org/nrg/xnatx/ohifviewer/OhifViewerPlugin.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @XnatPlugin(value = "ohifViewerPlugin",
             name = "XNAT OHIF Viewer Plugin",
-            version = "2.0.0",
+            version = "2.0.0-RC7",
             description = "Integrates the OHIF Cornerstone Viewer into XNAT.",
             openUrls = {"/viewer.html", "/*.js", "/*.stats.json", "/*.css", "/packages/**/*", "/sockjs/**/*"})
 @ComponentScan({
